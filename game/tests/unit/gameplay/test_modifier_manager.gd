1fa905e8969baa98177a875c16d8ad0344b77ccd
Manav Agarwal <257q1a0304@bgiic.ac.in>
2026-07-07 01:42:45 +0530

    test: clean up modifier manager unit nodes

--- game/tests/unit/gameplay/test_modifier_manager.gd
+++ game/tests/unit/gameplay/test_modifier_manager.gd
@@ -10,21 +10,21 @@ const ModifierManager := preload("res://src/gameplay/modifiers/modifier_manager.
 
 
 func test_active_id_empty_at_boot():
-    var m := ModifierManager.new()
+    var m: ModifierManager = autofree(ModifierManager.new())
     m._reload_tunables()
     m._build_pool()
     assert_eq(m.active_id(), "")
 
 
 func test_pool_contains_three_modifiers():
-    var m := ModifierManager.new()
+    var m: ModifierManager = autofree(ModifierManager.new())
     m._reload_tunables()
     m._build_pool()
     assert_eq(m._pool.size(), 3)
 
 
 func test_params_carry_scale_values():
-    var m := ModifierManager.new()
+    var m: ModifierManager = autofree(ModifierManager.new())
     m._reload_tunables()
     m._build_pool()
     var found: Dictionary = {"low_gravity": false, "slow_motion": false, "speed_burst": false}
